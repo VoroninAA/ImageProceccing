6b84177cb31b3248f01e7b1dcfdf9cdef7fb057e
KustovaAI <kustova_nastenka@list.ru>
2020-10-10 16:07:35 +0300

    Added time measurement

--- Lab1/task2/task2.py
+++ Lab1/task2/task2.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from Lab1.task1.metrics import *
 import cv2
+import time
 def my_average(img, img2):
     width, height = img.size
     for x in range(width):
@@ -9,20 +10,31 @@ def my_average(img, img2):
             New_color = round((R1 + G1 + B1) / 3)
             img2.putpixel((x, y), (New_color, New_color, New_color))
 
+# shows source image
 img = Image.open("firstimage.jpg")
+img.show()
+# shows average by me image
 img2 = img
+start_time_my_average = time.time()
 my_average(img, img2)
-
 opencvImage2 = cv2.cvtColor(np.array(img2), cv2.COLOR_RGB2BGR)  #convert from PIL to cv2
+time_my_average = time.time() - start_time_my_average
 cv2.imshow('my_averange', opencvImage2)
+
+# shows average by me cv
 img3 = cv2.imread("firstimage.jpg")
+start_time_cv_average = time.time()
 gray = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
 img3 = np.zeros_like(img)
 img3[:,:,0] = gray
 img3[:,:,1] = gray
 img3[:,:,2] = gray
+time_cv_average = time.time() - start_time_cv_average
 cv2.imshow('cv_GrayScale', img3)
 
-print(psnr(opencvImage2, img3))
-print("Изображения практически не отличаются")
+print("Схожесть изображений: ",psnr(opencvImage2, img3))
+print("По внешнему виду изображения практически практически не отличаются")
+print("Время выполнения конвертации по формуле: ", time_my_average)
+print("Время выполнения конвертации в OpenCV: ", time_cv_average)
+print("В OpenCV изображение обрабатывается гораздо быстрее.")
 cv2.waitKey()
--- Lab1/task3/task3.py
+++ Lab1/task3/task3.py
@@ -1,6 +1,6 @@
 import cv2
 from Lab1.task1.metrics import *
-
+import time
 
 def my_rgb_2_hsv(src):
     width = src.shape[0]
@@ -106,31 +106,56 @@ img = cv2.imread("img.jpg")
 cv2.imshow('original', img)
 
 # shows HSVed by me image
+start_time_my_HSV = time.time()
 my_rgb_2_hsv(img)
+time_my_HSV = time.time() - start_time_my_HSV
 cv2.imshow('HSV_my', img)
 
 # shows HSVed by CV image
 img2 = cv2.imread("img.jpg")
+start_time_cv_HSV = time.time()
 hsv_cv = cv2.cvtColor(img2, cv2.COLOR_RGB2HSV)
+time_cv_HSV = time.time() - start_time_cv_HSV
 cv2.imshow('HSV_cv', hsv_cv)
 
 print("Схожесть RGB -> HSV")
 print(psnr(img, hsv_cv))
+print("По внешнему виду изображения практически практически не отличаются")
+print("Время выполнения конвертации по формуле: ", time_my_HSV)
+print("Время выполнения конвертации в OpenCV: ", time_cv_HSV)
+print("В OpenCV изображение обрабатывается гораздо быстрее.")
 
 img3 = img
+start_time_my_RGB = time.time()
 my_hsv_2_rgb(img3)
+time_my_RGB = time.time() - start_time_my_RGB
 cv2.imshow("RGB_my", img3)
 
+start_time_cv_RGB = time.time()
 rgb_cv = cv2.cvtColor(hsv_cv, cv2.COLOR_HSV2RGB)
+time_cv_RGB = time.time() - start_time_cv_RGB
 cv2.imshow("RGB_cv", rgb_cv)
 print("Схожесть HSV -> RGB")
 print(psnr(rgb_cv, img3))
+print("По внешнему виду изображения практически практически не отличаются")
+print("Время выполнения конвертации по формуле: ", time_my_RGB)
+print("Время выполнения конвертации в OpenCV: ", time_cv_RGB)
+print("В OpenCV изображение обрабатывается гораздо быстрее.")
 
+start_time_brit_rgb = time.time()
 brightness_rgb(img3, 150)
+time_brit_rgb = time.time() - start_time_brit_rgb
 cv2.imshow("brightness_my", img3)
 
+start_time_brit_hsv = time.time()
 brightness_hsv(hsv_cv, 200)
 rgb_cv = cv2.cvtColor(hsv_cv, cv2.COLOR_HSV2RGB)
+time_brit_hsv = time.time() - start_time_brit_hsv
 cv2.imshow("brightness_hsv", rgb_cv)
+
+print("Схожесть яркости RGB and HSV")
+print(psnr(img3, rgb_cv))
+print("Время выполнения конвертации по RGB: ", time_brit_rgb)
+print("Время выполнения конвертации по HSV: ", time_brit_hsv)
 
 cv2.waitKey()
