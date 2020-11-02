a46ecd9df9847a18cc8ee09d71e76c3b44c4ebc8
Chesko <cheskoart9m@gmail.com>
2020-11-02 19:30:38 +0300

    corrected 1st task

--- Lab2/task1/task1.py
+++ Lab2/task1/task1.py
@@ -3,26 +3,36 @@ import numpy as np
 
 from ImageProceccing.Lab1.task1.metrics import *
 
-# TODO  refactor noise generation as im not sure if its right
+# d - deviation
+# sourceimage - s(i, j)
+def gaussian_noise2(sourceimage, d):
+    # calculating PDF (gaussian)
+    mean = np.mean(sourceimage)
+    K = 1 / d * np.sqrt(2 * np.pi)
+    prob = np.e ** (-(sourceimage - mean)**2 / (2*d*d)) * K
 
-def show_two_images(source_image, noised_image):
-    cv2.imshow('Result Image ', source_image)
-    cv2.imshow('Noised Image', noised_image)
-
-
-def gaussian_noise(sourceimage):
-    rand = np.random.normal(0, 9, size=[sourceimage.shape[0], sourceimage.shape[1]])
+    # calculating n(i, j) - simple noise
+    rand = np.random.normal(0, 20, size=[sourceimage.shape[0], sourceimage.shape[1], sourceimage.shape[2]])
     rand = np.uint8(rand)
-    noised_img = sourceimage + rand
-    noised_img[noised_img < 0] = 0
-    noised_img[noised_img > 255] = 255
-    return noised_img
 
+    # calucalatin f(i, j)
+    width = sourceimage.shape[0]
+    height = sourceimage.shape[1]
+    noised_image = sourceimage + 0
+    for x in range(width):
+        for y in range(height):
+            for i in range(3):
+                if np.random.random() * K < prob[x,y,i]:
+                    noised_image[x, y, i] += rand[x, y, i]
+
+    noised_image[noised_image < 0] = 0
+    noised_image[noised_image > 255] = 255
+    return noised_image
 
 if __name__ == "__main__":
-    image = cv2.imread('../images/testimage.jpg', 0)
-    noised_image = gaussian_noise(image)
+    image = cv2.imread('../images/testimage.jpg', 1)
+    noised_image = gaussian_noise2(image, 10)
     cv2.imshow('source_image', image)
     cv2.imshow('noised_image', noised_image)
-    cv2.waitKey(3000)
+    cv2.waitKey(30000)
 
